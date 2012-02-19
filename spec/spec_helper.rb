3eea7ef14f4ec585dd131b0b2fae72e38db89819
Michael Cooney <cooneymj27@uww.edu>
2012-02-18 22:16:26 -0600

    Please push

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -81,7 +81,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   # == Mock Framework
-  #
+  # Please push
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
   #
   # config.mock_with :mocha
